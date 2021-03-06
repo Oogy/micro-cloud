9a52023f828bbdf29eaa307aa092d04eb700f62a
oogy <tylerweldon94@gmail.com>
2021-03-06 16:40:34 -0500

    remove ssh key, can be done in playbook

--- init.sh
+++ init.sh
@@ -4,7 +4,6 @@ set -euxo pipefail
 
 APT_PACKAGES="python3 python3-pip python3-venv openssh-server git"
 PYTHON_PACKAGES="ansible"
-ADMIN_SSH_KEY="ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQC9yZlg7wYPxClZM5NumS8MonWPtuF85DasxbUBV9MUQVF7iDUBGmlnz2Cc5QZf5F27R5g+tm0CaghoT7V4FCH9AfuoslVR+8qtfCA2eCkJScQvBlUz1bhhtzqU8qH/tlcsZMaEj/NM+mo9OQYVUNx9nPghmUpVELIcOtxkPQn+YOS6kr6A10phf157HQcHgP91fgjMrncnCEYZhDq9+fRCNWGA6r3RZv683m3jFaYiS7sUDEGwIyFo/hc2HGpP/UTEbYOjHn+AyKCi5wK+9pPvCrFdiNcZrM6Y5FUiQLFLR9un9kczDhME2vLBkSU6FfuX1Ha6x8uqsZJK2cKIZNA4sn3xxti63I4UC/Q7CkJ8dLFFwA8wFq2RMnL19j1DvENI6dLLCRjPuqTTPcZmtoIaFdCCaRxD6CWL6c633NrMCzLQuvVkWU/AUYEUXTOjVVU3S1fl5nHGXo6ZwA3Ipxl6LTPFfipWv1A5/Y+yNJvhcSBH+bjGxuVuF/KmWatCS7tYXO4hdlELMlzcNdcjdFUlCGUf1fCgDOFaoViMFI1ccloCuxN9oNdWJFstU4DG6o6QInKDdELNqgJdAys6ExtSP0MbTnThJiKm5HVa7tII4bnJLWi7TUKhSILKwAwnkfCH121O846cB3st/GMR2hsFikUE98ra1K6G7pSYhaKMow=="
 
 apt_pkgs(){
     apt -y update
@@ -19,7 +18,6 @@ python_pkgs(){
 setup_ssh(){
     systemctl enable ssh
     systemctl start ssh
-    echo $ADMIN_SSH_KEY >> ~/.ssh/authorized_keys
     ufw allow ssh
     ufw enable
 }
